1ef5e5c541abda50490172a5a6dd4888cbee24e2
DaniloMarkicevic1 <danilo.markicevic1@gmail.com>
2021-10-08 16:33:29 +0200

    some js stuff

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,10 @@
 const theme1 = document.querySelector('.theme1');
 const theme2 = document.querySelector('.theme2');
 const theme3 = document.querySelector('.theme3');
+const input = document.querySelector('.inputScreen');
+const buttons = document.querySelectorAll('button');
 const body = document.querySelector('body');
+
 theme1.addEventListener('click', () => {
     body.classList.remove('light', 'dark');
     body.classList.add('neutral');
@@ -15,4 +18,25 @@ theme3.addEventListener('click', () => {
     body.classList.add('dark');
     body.classList.remove('light');
     body.classList.remove('neutral');
+});
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        switch (button.value) {
+            case 'reset':
+                input.value = '0';
+                break;
+            case '=':
+                break;
+            case 'x':
+                break;
+            case '+':
+                break;
+            case '-':
+                break;
+            case '/':
+                break;
+            default:
+                input.value += button.value;
+        }
+    });
 });
